d5af74e27b9ec0d16fe3270b3c8bbe3b7c7a3f3e
Calvin <luoyantao99@hotmail.com>
2020-05-04 13:24:25 -0400

    Verison 0.1
    ready for presentation

---
+++ Coronavirous Information/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Coronavirous Information
+//
+//  Created by Runhai Lin on 4/15/20.
+//  Copyright © 2020 Runhai Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+      
+    }
+
+
+}
+
