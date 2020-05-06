959fee87fe7a5c7f1f3b789758392949768f4fea
RunhaiLin <47149190+RunhaiLin@users.noreply.github.com>
2020-05-05 20:36:17 -0400

    Add files via upload

---
+++ Coronavirous Information/Coronavirous Information/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  Coronavirous Information
+//
+//  HomeViewController.swift
+//  HomeViewController is the first view controller when 
+//
+//  Created by Runhai Lin on 5/2/20.
+//  Copyright © 2020 Runhai Lin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var hometointro: UIButton!
+    @IBOutlet weak var Hometograph: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
